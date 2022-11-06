1a19705cd502453ca4b5303dcbfa86f33d08cd99
ChiSpe227 <113370753+ChiSpe227@users.noreply.github.com>
2022-11-05 21:35:32 -0400

    Week 9 Changing the Tilt
    Assigned to change the Green and Red balls of the tilt game.

---
+++ Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+        if (OtherBall.GetComponent<YellowBallCollisionManager>() != null)
+        {
+            Debug.Log("Blue Collided With Yellow");
+            
+            Destroy(OtherBall);
+
+        }
+       
+    }
+}
+
---
+++ Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+/*
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay())}(),nq}}")]
+public class CollisionColorChange : MonoBehaviour
+{
+
+    public Color orangecolor;
+    //public Color bluecolor;
+
+    
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("Red Collided with Yellow");
+            transform.GetComponent<Renderer>().material.color = orangecolor;
+        }
+    }
+
+    public void Global { get => global; set => this.global = value; }
+}
+*/
+
+    /*
+    public class RedBallCollisionManager : BallCollisionManager
+    {
+            public color orangecolor;
+
+        public override void CollideWithBall(GameObject OtherBall)
+        {
+            Debug.Log("Red Ball Collision Manager Function");
+
+            if (OtherBall.GetComponent<YellowBallCollisionManager>() != null)
+            {
+                Debug.Log("Red Collided With Yellow");
+                    transform.GetComponent<Render>().material.color = orangecolor;
+            }
+        }
+    }
+    */
+
+    /*
+    public class Colour : MonoBehavior
+    {
+        void OncollisionEnter(Collision col)
+        {
+            if(col.gameObject.tag == "Ball")
+            {
+                object p = col.gameObject.GetComponent<Renderer>(); material.color = Color.orange;
+            }
+        }
+    }
+    */
